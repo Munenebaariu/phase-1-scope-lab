d4c534fb358df09e545e6c82838b2466c988e5d0
Munene Baariu <munenebaariu@gmail.com>
2025-10-15 15:07:58 +0300

    Completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+// Global variable
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newFavorite) {
+  window.bestCustomer = newFavorite;
+}
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not bob';
+}
+
+// Attach all to window for mocha/jsdom to see them
+window.upperCaseCustomerName = upperCaseCustomerName;
+window.setBestCustomer = setBestCustomer;
+window.overwriteBestCustomer = overwriteBestCustomer;
+window.changeLeastFavoriteCustomer = changeLeastFavoriteCustomer;
+window.leastFavoriteCustomer = leastFavoriteCustomer;
